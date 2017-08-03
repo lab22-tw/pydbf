19e9d1b3e0648d955011ae2ae01cd449ef07a900
Kevin Chen <me@kevchentw.com>
2017-08-03 12:12:34 +0800

    fork and update

--- setup.py
+++ setup.py
@@ -12,22 +12,14 @@ if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
-elif sys.argv[-1] == "test":
-    os.system("./run_tests.py")
-    sys.exit()
-
-elif sys.argv[-1] == "docs":
-    os.system("sphinx-build docs/ docs/_build")
-    sys.exit()
-
 setup(
-    name='dbfread',
-    version=dbfread.__version__,
+    name='pydbf',
+    version=pydbf.__version__,
     description='Read DBF Files with Python',
     long_description=open('README.rst', 'rt').read(),
-    author=dbfread.__author__,
-    author_email=dbfread.__email__,
-    url=dbfread.__url__,
+    author=pydbf.__author__,
+    author_email=pydbf.__email__,
+    url=pydbf.__url__,
     package_data={'': ['LICENSE']},
     package_dir={'dbfread': 'dbfread'},
     packages = ['dbfread'],
